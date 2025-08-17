728cc0667879d49cd7ef413901a29f0454a9a4dd
ScaryEvilTomato <omglol9898719@gmail.com>
2025-08-17 20:29:15 +0300

    Reworked comparison

--- quadratic_solver(1).cpp
+++ quadratic_solver(1).cpp
@@ -12,40 +12,52 @@ bool linear_solve(double a, double b, double c, int *type_of_answer, double *x1)
 void equation_solve(double a1, double b1, double c1, int *type_of_answer, double *x1, double *x2);
 void print_answer(int type_of_answer, double x1, double x2);
 void solver();
- 
-void normalize_pow2(double *a, double *b, double *c) {
-    if (*a == 0.0 && *b == 0.0 && *c == 0.0) return;
+bool is_zero(double a);
+bool is_zero_by_memcmp(double a);
 
-    int ea = (*a != 0.0) ? ilogb(*a) : 0;
-    int eb = (*b != 0.0) ? ilogb(*b) : 0;
-    int ec = (*c != 0.0) ? ilogb(*c) : 0;
+
+bool is_zero(double a) {
+    return fabs(a) < DBL_EPSILON;
+}
+
+bool is_zero_by_memcmp(double a) {
+    return a == 0.0;
+}
+
+
+void normalize_pow2(double *a, double *b, double *c) {
+    if (is_zero_by_memcmp(*a) && is_zero_by_memcmp(*b) && is_zero_by_memcmp(*c)) return;
+
+    int ea = (!is_zero_by_memcmp(*a)) ? ilogb(*a) : 0;
+    int eb = (!is_zero_by_memcmp(*b)) ? ilogb(*b) : 0;
+    int ec = (!is_zero_by_memcmp(*c)) ? ilogb(*c) : 0;
 
 
     int t_min = EMIN; 
     int t_max = EMAX; 
 
 
-    if (*b != 0.0) {
-        int n  = EMAX - 2*eb;
-        int hi = (n >= 0) ? (n/2) : ((n - 1)/2); 
+    if (!is_zero_by_memcmp(*b)) {
+        int n  = EMAX - 2 * eb;
+        int hi = (n >= 0) ? (n / 2) : ((n - 1) / 2); 
         if (hi < t_max) t_max = hi;
     }
-    if (*a != 0.0 && *c != 0.0) {
+    if (!is_zero_by_memcmp(*a) && !is_zero_by_memcmp(*c)) {
 
         int n  = EMAX - 2 - ea - ec;
-        int hi = (n >= 0) ? (n/2) : ((n - 1)/2); 
+        int hi = (n >= 0) ? (n / 2) : ((n - 1) / 2); 
         if (hi < t_max) t_max = hi;
     }
 
 
-    if (*b != 0.0) {
-        int n  = EMIN - 2*eb;
-        int lo = (n >= 0) ? ((n + 1)/2) : (n/2);
+    if (!is_zero_by_memcmp(*b)) {
+        int n  = EMIN - 2 * eb;
+        int lo = (n >= 0) ? ((n + 1) / 2) : (n / 2);
         if (lo > t_min) t_min = lo;
     }
-    if (*a != 0.0 && *c != 0.0) {
+    if (!is_zero_by_memcmp(*a) && !is_zero_by_memcmp(*c)) {
         int n  = EMIN - 2 - ea - ec;
-        int lo = (n >= 0) ? ((n + 1)/2) : (n/2);   
+        int lo = (n >= 0) ? ((n + 1) / 2) : (n / 2);   
         if (lo > t_min) t_min = lo;
     }
 
@@ -55,6 +67,7 @@ void normalize_pow2(double *a, double *b, double *c) {
     *b = scalbn(*b, t);
     *c = scalbn(*c, t);
 }
+
 
 bool input(double *a, double *b, double *c) {
     printf("Введите коэффициенты:\n");
@@ -66,9 +79,9 @@ bool input(double *a, double *b, double *c) {
 }
 
 bool linear_solve(double a, double b, double c, int *type_of_answer, double *x1) {
-    if (a == 0.0) {
-        if (b == 0.0) {
-            if (c == 0.0) *type_of_answer = -1;
+    if (is_zero(a)) {
+        if (is_zero(b)) {
+            if (is_zero(c)) *type_of_answer = -1;
             else *type_of_answer = 0;
             return true;
         } else {
@@ -108,11 +121,9 @@ void equation_solve(double a, double b, double c, int *type_of_answer, double *x
 
     double D = b * b - 4.0 * a * c;
 
-    double tolD = DBL_EPSILON * (fabs(b) * fabs(b) + 4.0 * fabs(a) * fabs(c));
-
-    if (D > tolD) {
+    if (D > DBL_EPSILON) {
         double temp = -0.5 * (b + sqrt(D)); 
-        if (temp == 0.0) {
+        if (is_zero(temp)) {
             *x1 = (-0.5 * b) / a;   
             *type_of_answer = 1;
         } else {
@@ -120,7 +131,7 @@ void equation_solve(double a, double b, double c, int *type_of_answer, double *x
             *x2 = c / temp;
             *type_of_answer = 2;
         }
-    } else if (fabs(D) < tolD) {
+    } else if (is_zero(D)) {
         *x1 = (-0.5 * b) / a;   
         *type_of_answer = 1;
     } else {
