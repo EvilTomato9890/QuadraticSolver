97228e21c2dcbd85cc30821b1ddc3909caa02a9f
ScaryEvilTomato <omglol9898719@gmail.com>
2025-08-17 21:43:51 +0300

    Updated2 cmp_to_zero

--- quadratic_solver(1).cpp
+++ quadratic_solver(1).cpp
@@ -26,23 +26,23 @@ int cmp_to_zero(double a) {
 }
 
 void normalize_pow2(double *a, double *b, double *c) {
-    if (cmp_to_zero(*a) != 0 && cmp_to_zero(*b) != 0 && cmp_to_zero(*c) != 0) return;
+    if (*a != 0.0 && *b != 0.0 && *c != 0.0) return;
 
-    int ea = (cmp_to_zero(*a) != 0) ? ilogb(*a) : 0;
-    int eb = (cmp_to_zero(*b) != 0) ? ilogb(*b) : 0;
-    int ec = (cmp_to_zero(*c) != 0) ? ilogb(*c) : 0;
+    int ea = (*a != 0.0) ? ilogb(*a) : 0;
+    int eb = (*b != 0.0) ? ilogb(*b) : 0;
+    int ec = (*c != 0.0) ? ilogb(*c) : 0;
 
 
     int t_min = EMIN; 
     int t_max = EMAX; 
 
 
-    if (cmp_to_zero(*b) != 0) {
+    if (*b != 0.0) {
         int n  = EMAX - 2 * eb;
         int hi = (n >= 0) ? (n / 2) : ((n - 1) / 2); 
         if (hi < t_max) t_max = hi;
     }
-    if (cmp_to_zero(*a) != 0 && cmp_to_zero(*c) != 0) {
+    if (*a != 0.0 && *c != 0.0) {
 
         int n  = EMAX - 2 - ea - ec;
         int hi = (n >= 0) ? (n / 2) : ((n - 1) / 2); 
@@ -50,12 +50,12 @@ void normalize_pow2(double *a, double *b, double *c) {
     }
 
 
-    if (cmp_to_zero(*b) != 0) {
+    if (*b != 0.0) {
         int n  = EMIN - 2 * eb;
         int lo = (n >= 0) ? ((n + 1) / 2) : (n / 2);
         if (lo > t_min) t_min = lo;
     }
-    if (cmp_to_zero(*a) != 0 && cmp_to_zero(*c) != 0) {
+    if (*a != 0.0 && *c != 0.0) {
         int n  = EMIN - 2 - ea - ec;
         int lo = (n >= 0) ? ((n + 1) / 2) : (n / 2);   
         if (lo > t_min) t_min = lo;
